783376b9b1175af511ad812451770075ea87e0b4
Walter Ontivero <wontivero@gmail.com>
2025-08-24 18:22:53 -0300

    Actualice el CUIT

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ export const companyInfo = {
     name: "",
     address: "Av. Revolución de Mayo 1806. Córdoba, Argentina.",
     phone: "351-7693065",
-    cuit: "27-25610417-8",
+    cuit: "20-30843660-9",
     ivaCondition: "Monotributista",
     email: "consulta.infotech@gmail.com",
     logoUrl: "img/logo.png" // <--- Aquí va la ruta a tu logo
